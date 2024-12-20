0db1a51cbbc3e0da992cead5fb2b3dff7111a9cf
fdelgadov <fdelgadov@unsa.edu.pe>
2024-12-20 01:09:50 -0500

    feat(sensor-data): se completa el service

---
+++ pmv-fe/src/app/services/sensor-data.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { GlobalService } from './global.service';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SensorDataService {
+  endpoint = `${this.gs.rootPath}/sensorData`
+  
+  constructor(
+    private gs: GlobalService,
+    private http: HttpClient,
+  ) { }
+
+  findLastData(){
+    return this.http.get(`${this.endpoint}/lastData`)
+  }
+
+  findAllData(){
+    return this.http.get(`${this.endpoint}/all`)
+  }
+}
